390515e650e9dd8527094368cab76d4dc55edf31
Coders.Mickey <thuongnguyen.it78@gmail.com>
2021-06-05 14:18:04 +0700

    Initial commit

---
+++ src/model/Board.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Board {
+	private int[] board;
+	private int count = 0;
+	private Point lastPoint;
+	
+}
---
+++ src/model/Point.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Point {
+	private int x;
+	private int y;
+	private int playerId;
+}
